c94efa842ede52653c9e6ab94edec68fbe64e83c
丶弛 <wanghaichi@twtstudio.com>
2016-04-28 22:36:58 +0800

    judge sentinel kind by Hardy

---
+++ src/main/java/cn/tjuscs/oj/whc/KindEOF.java
@@ -0,0 +1,17 @@
+package cn.tjuscs.oj.whc;
+
+
+public class KindEOF extends FileKind{
+
+	public KindEOF(String sourcePath, String targetPath, String rightProPath) {
+		super(sourcePath, targetPath, rightProPath);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	boolean splitFile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
