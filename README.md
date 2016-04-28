a0e1fa44095c2a9a4c921736af3094d990cfffa2
武铎 <wuduotju@163.com>
2016-04-28 20:13:58 +0800

    Revert "dividing for T case"
    This reverts commit 5e178beabb7060e15329b914d0d76547a9b7a077.

---
+++ README.md
@@ -0,0 +1,2 @@
+# TOJ_Plus_Plus
+这是一个toj项目
---
+++ src/main/java/cn/tjuscs/oj/testCases/Main.java
@@ -0,0 +1,30 @@
+package cn.tjuscs.oj.testCases;
+
+public class Main {
+	
+	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//		String str = "";
+//		File file = new File("haha.txttt");
+//		FileInputStream fileInputStream = null;
+//		fileInputStream = new FileInputStream(file);
+//		InputStreamReader reader = new InputStreamReader(fileInputStream);
+//		// char[] cbuf = new char[10000];
+//		// reader.read(cbuf);
+//		// str = new String(cbuf);
+//		// System.out.println(cbuf);
+//		BufferedReader bufferReader = new BufferedReader(reader);
+//		Scanner cin = new Scanner(fileInputStream);
+//		while (cin.hasNext()) {
+//
+//		}
+		
+		SplitTestCases mySplit = null;
+		
+		
+		mySplit.init("", "", "");
+		mySplit.getSplitInputFilesResult();
+		mySplit.getSplitOutputFilesResult();
+	}
+
+}
