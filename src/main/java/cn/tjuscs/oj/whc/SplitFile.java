d96047787d0a33868f620afb88ffaa5cc951d539
ncfxy <359419163@qq.com>
2016-04-28 22:44:52 +0800

    消除splitFile的编译错误

--- src/main/java/cn/tjuscs/oj/whc/SplitFile.java
+++ src/main/java/cn/tjuscs/oj/whc/SplitFile.java
@@ -5,18 +5,15 @@ import java.util.List;
 public class SplitFile implements SplitTestCases {
 
 	
-	@Override
 	public void init(String inputFilePath, String outputFilePath, String rightProgramPath) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public List<String> getSplitInputFilesResult() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<String> getSplitOutputFilesResult() {
 		// TODO Auto-generated method stub
 		return null;
