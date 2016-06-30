66c11ca049c444206aeb8fb5e77975045dabc0a6
王月欢 <2386458640@qq.com>
2016-06-30 21:25:17 +0800

    比较文件

--- src/main/java/cn/tjuscs/oj/wyh/QueryAndCompare.java
+++ src/main/java/cn/tjuscs/oj/wyh/QueryAndCompare.java
@@ -1,11 +1,12 @@
 package cn.tjuscs.oj.wyh;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 
 public class QueryAndCompare {
 	public static void main(String [] args){
-		DB db = new DB("root", "root", "toj");
+		DB db = new DB("root", "", "toj");
 		db.setTable("judge");
 		db.getByUid("temp0");
 		//db.setLimit(2);
@@ -20,9 +21,8 @@ public class QueryAndCompare {
 		String file1 = list1.get(0);
 		String file2 = list2.get(0);
 		
-		String url = "F:\\Material\\2016dachaung\\acmdata\\src\\";
-		String str = CompareFile.compare(url + file1 ,  url + file2);
-		System.out.println(str);
+		CompareFile compareFile = new CompareFile(file1, file2);
+		compareFile.judge();
 	}
 }
  
