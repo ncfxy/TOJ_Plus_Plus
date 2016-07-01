86cfcb3e787f865621273765fccb93842373f021
ncfxy <359419163@qq.com>
2016-07-01 22:00:21 +0800

    去掉GBK转码

--- src/main/java/cn/tjuscs/oj/cmdHelper/StreamGobbler.java
+++ src/main/java/cn/tjuscs/oj/cmdHelper/StreamGobbler.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.security.PublicKey;
 import java.util.logging.Level;
 
 /**
@@ -43,7 +42,7 @@ public class StreamGobbler extends Thread {
 				printWriter = new PrintWriter(_outputStream);
 			}
 
-			inputStreamReader = new InputStreamReader(_inputStream, Charset.forName("GBK"));
+			inputStreamReader = new InputStreamReader(_inputStream);
 			bufferReader = new BufferedReader(inputStreamReader);
 			readFromInputStream(bufferReader, printWriter);
 		} catch (IOException e) {
