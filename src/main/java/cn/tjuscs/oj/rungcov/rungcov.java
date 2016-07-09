835cdfb8a570ffd2c7a703844799c232e2e99ac0
Shi Fengmin <544814745@qq.com>
2016-07-09 12:28:46 +0800

    修改gcov

--- src/main/java/cn/tjuscs/oj/rungcov/rungcov.java
+++ src/main/java/cn/tjuscs/oj/rungcov/rungcov.java
@@ -1,5 +1,4 @@
 package cn.tjuscs.oj.rungcov;
-
 import java.util.*;
 
 import cn.tjuscs.oj.cmdHelper.ExecuteWindowsCommand;
@@ -7,21 +6,27 @@ import cn.tjuscs.oj.cmdHelper.ExecuteWindowsCommand;
 import java.io.*;
 
 public class rungcov {
-	public static void main(String [] argvs) throws NumberFormatException, IOException{
-		String casenumFileName = "caseNum.txt";
-		String inputFileName = "test";
-		String outputFileName = "output";
-		String srcFileName = "demo";
+	public static void runAndGetMat(int sid, int pid) throws NumberFormatException, IOException{
+		System.out.println("haha");
+		//sid = "1131123";
+		//pid = "1007";
+		String dataPath = "./data/toj_problem"+pid;
+		String casenumFileName = dataPath+"/splitedTestCases/caseNum.in";
+		String inputFileName = dataPath+"/splitedTestCases/test";
+		String outputFileName = dataPath+"/splitedTestCases/output";
+		String srcFileName = dataPath+"/programs/commit_id_"+sid+"/"+sid;
 		FileReader cnst = new FileReader(casenumFileName);
 		BufferedReader fin = new BufferedReader(cnst);
 		int casenum = Integer.valueOf(fin.readLine()).intValue();
 		ExecuteWindowsCommand CMD = null;
 		ExecuteWindowsCommand.execute("g++ "+srcFileName+".cpp -o "+srcFileName + ".exe -ftest-coverage -fprofile-arcs");
 		for(int i=1;i<=casenum;i++){
-			ExecuteWindowsCommand.execute(srcFileName+".exe < "+inputFileName+i+".txt > "+outputFileName+i+".txt");
+			ExecuteWindowsCommand.execute(srcFileName+".exe < "+inputFileName+i+".in > "+outputFileName+i+".out");
 			ExecuteWindowsCommand.execute("gcov "+srcFileName+".cpp");
 			ExecuteWindowsCommand.execute("ren "+srcFileName+".cpp.gcov "+srcFileName+i+".cpp.gcov");
-		}   
-		CMD.execute("python newgetMatrixFromGcov.py "+srcFileName+" 1 "+casenum);
+		}
+		
+		ExecuteWindowsCommand.execute("python ./pyscripts/compfile.py "+(dataPath+"/programs/commit_id_"+sid+"/outputs.csv ")+(dataPath+"/splitedTestCases/ans")+(outputFileName)+casenum);
+		ExecuteWindowsCommand.execute("python ./pyscripts/getMatrixFromGcov.py "+srcFileName+" 1 "+casenum);
 	}
 }
