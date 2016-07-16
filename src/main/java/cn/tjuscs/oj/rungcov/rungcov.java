e0d8d8652cb7793fd65c4e2403af7ffb72fbeb38
武铎 <wuduotju@163.com>
2016-07-16 22:34:16 +0800

    石丰民在武铎这改了一点东西，没改好，我传上去给他继续改“

--- src/main/java/cn/tjuscs/oj/rungcov/rungcov.java
+++ src/main/java/cn/tjuscs/oj/rungcov/rungcov.java
@@ -16,22 +16,29 @@ public class rungcov {
 		//pid = "1007";
 		String dataPath = "./data/toj_problem_"+pid;
 		dataPath = new File(dataPath).getCanonicalPath();
-		String casenumFileName = dataPath+"/splitedTestCases/2800_total.txt";
-		String inputFileName = dataPath+"/splitedTestCases/2800_";
+		String casenumFileName = dataPath+"/splitedTestCases/" + pid + "_total.txt";
+		String inputFileName = dataPath+"/splitedTestCases/" + pid + "_";
 		String outputFileName = dataPath+"/splitedTestCases/output";
 		String srcFileName = dataPath+"/programs/commit_id_"+sid+"/"+sid;
 		FileReader cnst = new FileReader(casenumFileName);
 		BufferedReader fin = new BufferedReader(cnst);
 		int casenum = Integer.valueOf(fin.readLine()).intValue();
 		ExecuteWindowsCommand CMD = null;
-		ExecuteWindowsCommand.execute("g++ "+srcFileName+".cpp -o "+srcFileName + ".exe -ftest-coverage -fprofile-arcs");
-		for(int i=1;i<=casenum;i++){
-			ExecuteWindowsCommand.execute(srcFileName+".exe < "+inputFileName+i+".in > "+outputFileName+i+".out");
-			ExecuteWindowsCommand.execute("gcov "+srcFileName+".cpp");
-			ExecuteWindowsCommand.execute("ren "+srcFileName+".cpp.gcov "+srcFileName+i+".cpp.gcov");
+		ExecuteWindowsCommand.execute("cd "+dataPath+"\\programs\\commit_id_testid\n" + "g++ "+srcFileName+".cpp -o "+srcFileName + ".exe -ftest-coverage -fprofile-arcs");
+		for(int i=0;i<casenum;i++){
+			ExecuteWindowsCommand.execute("cd "+dataPath+"\\programs\\commit_id_testid\n" + srcFileName+".exe < "+inputFileName+i+".in > "+outputFileName+i+".out");
+			ExecuteWindowsCommand.execute("cd "+dataPath+"\\programs\\commit_id_testid\n" + "gcov "+srcFileName+".cpp");
+//			try{
+//			    Thread thread = Thread.currentThread();
+//			    thread.sleep(1500);//暂停1.5秒后程序继续执行
+//			}catch (InterruptedException e) {
+//			    // TODO Auto-generated catch block
+//			    e.printStackTrace();
+//			} 
+			ExecuteWindowsCommand.execute("cd "+dataPath+"\\programs\\commit_id_testid\n" + "ren "+sid+".cpp.gcov "+sid+i+".cpp.gcov");
 		}
-		
-		ExecuteWindowsCommand.execute("python ./linux和python脚本/compfile.py "+(dataPath+"/programs/commit_id_"+sid+"/outputs.csv ")+(dataPath+"/2800.out")+(outputFileName)+casenum);
-		ExecuteWindowsCommand.execute("python ./linux和python脚本/getMatrixFromGcov.py "+srcFileName+" 1 "+casenum);
+		System.out.println("hah");
+		ExecuteWindowsCommand.execute("python G:\\SI\\oj\\TOJ_Plus_Plus/linux和python脚本/compfile.py "+(dataPath+"/programs/commit_id_"+sid+"/outputs.csv ")+(dataPath+"/" + pid + ".out ")+(outputFileName)+casenum);
+		ExecuteWindowsCommand.execute("python G:\\SI\\oj\\TOJ_Plus_Plus./linux和python脚本/getMatrixFromGcov.py "+srcFileName+" 1 "+casenum);
 	}
 }
